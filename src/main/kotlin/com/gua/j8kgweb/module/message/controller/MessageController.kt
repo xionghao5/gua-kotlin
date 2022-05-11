42e409604263966c89cb334067070d65c9d6af3e
xionghao <Erik.Xiong@cn.ey.com>
2022-05-11 14:47:09 +0800

    swagger补全遗漏的接口

--- src/main/kotlin/com/gua/j8kgweb/module/message/controller/MessageController.kt
+++ src/main/kotlin/com/gua/j8kgweb/module/message/controller/MessageController.kt
@@ -2,18 +2,23 @@ package com.gua.j8kgweb.module.message.controller
 
 import com.gua.j8kgweb.module.message.pojo.Message
 import com.gua.j8kgweb.module.message.service.MessageService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Api(tags = ["消息接口"])
 @RestController
 @RequestMapping("/message")
 class MessageController(val service: MessageService) {
+    @ApiOperation("获取全部消息")
     @GetMapping("/getAll")
     fun getAll(): List<Message> {
         return service.getAll()
     }
 
+    @ApiOperation("新增消息")
     @GetMapping("/addOne")
     fun addOne() {
         service.addOne()
